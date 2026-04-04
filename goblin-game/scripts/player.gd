78288e9baf349f347f22ce506f48951f30358c21
Hassaan-Naveed <hassaann688@gmail.com>
2026-04-04 18:14:47 +0100

    controller jump + tweaking jump height

--- goblin-game/scripts/player.gd
+++ goblin-game/scripts/player.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -1000.0
-var can_move = true	
+const JUMP_VELOCITY = -800.0
+var can_move = true
 
 func _physics_process(delta: float) -> void:
 	
@@ -18,9 +18,9 @@ func _physics_process(delta: float) -> void:
 		# Handle jump.
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
-			
+
 		if Input.is_action_just_released("ui_accept") and velocity.y < 0:
-			velocity.y *= 0.5 
+			velocity.y *= 0.5
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
