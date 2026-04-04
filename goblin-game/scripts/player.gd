89ab6a7d9c8eb74f7cd1d70245b32f1d4a314d39
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2026-04-04 18:06:10 +0100

    Change player scale

--- goblin-game/scripts/player.gd
+++ goblin-game/scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -1000.0
+var can_move = true	
 
 func _physics_process(delta: float) -> void:
 	
@@ -13,19 +14,20 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += current_gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
-		velocity.y *= 0.5 
+	if can_move:
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			
+		if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+			velocity.y *= 0.5 
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
