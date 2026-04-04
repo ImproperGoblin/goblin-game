42ad278802bd8368d0b55ea2acc64b16f2ece1e8
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2026-04-04 17:53:07 +0100

    Level management

--- goblin-game/scripts/main.gd
+++ goblin-game/scripts/main.gd
@@ -1,25 +1,33 @@
 extends Node2D
 var level: int = 1
+var current_level_root: Node = null
+var level_root: Node = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Set up Level
 	current_level_root = get_node("LevelRoot")
-	_setup_level()
-
+	_load_level(level)
 
 # -----------------
 # LEVEL MANAGEMENT
 # -----------------
 
 func _load_level(level_number:int) -> void:
+	if current_level_root:
+		current_level_root.queue_free()
 	
-
-# Level Setup
-func _setup_level() -> void:
+	# Change level
+	var level_path = "res://scenes/levels/lush_level_%s.tscn" %level_number
+	current_level_root = load(level_path).instantiate()
+	add_child(current_level_root)
+	current_level_root.name = "LevelRoot"
+	_setup_level(current_level_root)
+	
+func _setup_level(level_root: Node) -> void:
 	
 	# Connect EXIT
-	var exit = $LevelRoot.get_node_or_null("Exit")
+	var exit = level_root.get_node_or_null("Exit")
 	if exit:
 		exit.body_entered.connect(_on_exit_body_entered)
 
@@ -31,3 +39,4 @@ func _on_exit_body_entered(body: Node2D) -> void:
 		level += 1
 		print (level)
 		body.can_move = false
+		_load_level(level)
