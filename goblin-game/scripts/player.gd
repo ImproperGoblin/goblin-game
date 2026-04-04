10102d50eed62207d58a832d674c9fe364bf936d
Hassaan-Naveed <hassaann688@gmail.com>
2026-04-04 18:03:09 +0100

    updating jump physics

--- goblin-game/scripts/player.gd
+++ goblin-game/scripts/player.gd
@@ -1,42 +1,31 @@
 extends CharacterBody2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -700.0
-var alive = true
-var can_move = true
+const JUMP_VELOCITY = -1000.0
 
 func _physics_process(delta: float) -> void:
 	
-	# Add animation
-	if velocity.x > 1 or velocity.x < -1:
-		animated_sprite_2d.animation = "running"
-	else:
-		animated_sprite_2d.animation = "idle"
-	
+	var gravity = get_gravity()
+	var current_gravity = gravity if velocity.y < 0 else gravity * 1.5
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += current_gravity * delta
 
-	if can_move:
-		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-			animated_sprite_2d.animation = "jump"
-
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-
-		move_and_slide()
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 		
-		if direction == 1.0:
-			animated_sprite_2d.flip_h = false
-		elif direction == -1.0:
-			animated_sprite_2d.flip_h = true
+	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+		velocity.y *= 0.5 
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
