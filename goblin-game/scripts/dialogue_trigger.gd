5008d7fb0c97fcc67e5917dc76b9e3afb3bd63b5
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2026-04-04 22:25:07 +0100

    Dialogue

---
+++ goblin-game/scripts/dialogue_trigger.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func _ready() -> void:
+	$Label.visible_ratio = 0
+
+	
+func _display_dialogue() -> void:
+	var dialogue_tween := create_tween()
+	dialogue_tween.tween_property($Label, "visible_ratio", 1, 1.5)
+	await dialogue_tween.finished
