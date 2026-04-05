af1c49a263f6a97777d5ce13bce9506201a542e5
Hassaan-Naveed <hassaann688@gmail.com>
2026-04-05 06:19:31 +0100

    pause menu

---
+++ goblin-game/scripts/pause_menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	print('hello')
+	if event.is_action_pressed('pause'):
+		if get_tree().paused:
+			_resume_game()
+		else:
+			_pause_game()
+
+func _pause_game():
+	get_tree().paused = true
+	show()
+
+func _resume_game():
+	hide()
+	get_tree().paused = false
+
+
+func _on_button_continue_pressed() -> void:
+	_resume_game()
+
+func _on_button_quit_pressed() -> void:
+	get_tree().quit()
