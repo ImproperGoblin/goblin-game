d1f8a4fd18fff9ac2209d509e3d7949b5bf63677
Hassaan-Naveed <hassaann688@gmail.com>
2026-04-05 04:21:52 +0100

    improving shake

--- goblin-game/scripts/player_camera.gd
+++ goblin-game/scripts/player_camera.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
 const SHAKE_FADE: float = 20.0
+const SHAKE_SMOOTHING: float = 0.4
 
 var shake_strength: float = 0.0
 
@@ -9,14 +10,14 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if shake_strength > 0:
-		offset = Vector2(
+		var target_offset := Vector2(
 			randf_range(-shake_strength, shake_strength),
 			randf_range(-shake_strength, shake_strength)
 		)
-		
+		offset = offset.lerp(target_offset, SHAKE_SMOOTHING)
 		shake_strength = move_toward(shake_strength, 0.0, SHAKE_FADE * delta)
 	else:
-		offset = Vector2.ZERO
+		offset = offset.lerp(Vector2.ZERO, SHAKE_SMOOTHING)
 
 func apply_shake(strength: float) -> void:
 	shake_strength = max(shake_strength, strength)
