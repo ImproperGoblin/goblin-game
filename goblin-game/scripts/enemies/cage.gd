3e193f0b3ac710374eafbf08ae363e246bb5b28e
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2026-04-05 16:23:35 +0100

    Remove cage on success

--- goblin-game/scripts/enemies/cage.gd
+++ goblin-game/scripts/enemies/cage.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		GameState._set_key_status()
-		self.queue_free()
+		if GameState._get_key_status():
+			self.queue_free()
