3883df94335445f8c976fb4a3322c330f26b4fae
Hassaan-Naveed <hassaann688@gmail.com>
2026-04-05 01:48:09 +0100

    fixing stuff

--- goblin-game/scripts/enemies/venus_fly_trap.gd
+++ goblin-game/scripts/enemies/venus_fly_trap.gd
@@ -1,30 +1,31 @@
 extends Node2D
 
-@onready var player_node: CharacterBody2D = $"../Player"
+const SNAP_TIME: float = 2.0
 
-const SNAP_TIME = 2
-
-var increase_snap_timer = false
-var snap_timer = 0
+var player_node: Node = null
+var increase_snap_timer: bool = false
+var snap_timer: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if increase_snap_timer:
 		snap_timer += delta
 		
-	if snap_timer > SNAP_TIME:
+	if player_node and snap_timer > SNAP_TIME:
 		player_node._reset_to_safe_pos()
-		
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		snap_timer = 0
 		increase_snap_timer = true
+		player_node = body
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		snap_timer = 0
 		increase_snap_timer = false
+		player_node = null
--- goblin-game/scripts/player.gd
+++ goblin-game/scripts/player.gd
@@ -2,29 +2,32 @@ extends CharacterBody2D
 
 @onready var hazard_tilemap: TileMapLayer = $"../LushHazardTileMap"
 
-const MAX_SPEED = 400.0
-const ACCELERATION = 1200.0
-const FRICTION = 1400.0
-const JUMP_VELOCITY = -800.0
-const COYOTE_TIME_LENGTH = 0.1
-const JUMP_BUFFER_MIN = 0.2
+const MAX_SPEED: float = 400.0
+const ACCELERATION: float = 1200.0
+const FRICTION: float = 1400.0
+const JUMP_VELOCITY: float = -800.0
+const COYOTE_TIME_LENGTH: float = 0.1
+const JUMP_BUFFER_MIN: float = 0.2
 
-var last_safe_coords = 0
-var room_start_coordinates = 0
-var can_move = true
-var coyote_timer = 0
-var jump_buffer_timer = 0
-var buffered_jump = false
-var jump_boost = 1
+var last_safe_coords: Vector2 = Vector2(0.0, 0.0)
+var room_start_coordinates: Vector2 = Vector2(0.0, 0.0)
+var can_move: bool = true
+var coyote_timer: float = 0.0
+var jump_buffer_timer: float = 0.0
+var buffered_jump: bool = false
+var jump_boost: float = 1.0
 
-func _ready() -> void:
+func _ready() -> void:	
 	last_safe_coords = global_position
 	room_start_coordinates = global_position
+
+	for ft in get_tree().get_nodes_in_group("flytrap"):
+		ft.flytrap_damage_player.connect(_on_flytrap_damage)
 
 func _physics_process(delta: float) -> void:
 	var gravity = get_gravity()
 	var current_gravity = gravity if velocity.y < 0 else gravity * 1.5
-	
+
 	if is_on_floor():
 		if $JumpRay.is_colliding():
 			_set_jump_boost(1)
@@ -85,3 +88,7 @@ func _reset_to_safe_pos() -> void:
 
 func _set_safe_pos() -> void:
 	last_safe_coords = global_position
+	
+func _on_flytrap_damage(player) -> void:
+	if player == self:
+		_reset_to_safe_pos()
