44559bf4e987a0139b520ebc744be4306d0ab128
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2026-04-05 16:14:41 +0100

    Key collecting mechanism

--- goblin-game/scripts/game_state.gd
+++ goblin-game/scripts/game_state.gd
@@ -2,6 +2,7 @@ extends Node
 
 const __MAX_PLAYER_HP: int = 6
 
+var __key_acquired: bool = false
 var __player_hp: int = __MAX_PLAYER_HP
 
 func _ready() -> void:
@@ -16,6 +17,12 @@ func _get_player_hp() -> int:
 func _reduce_player_hp(hp_to_reduce: int = 1) -> int:
 	__player_hp = max(__player_hp - hp_to_reduce, 0)
 	return __player_hp
+
+func _get_key_status() -> bool:
+	return __key_acquired
+
+func _set_key_status() -> void:
+	__key_acquired = !__key_acquired
 
 func _reset_game_state():
 	__player_hp = __MAX_PLAYER_HP
